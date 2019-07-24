00d144c60280eb92a0f11766802952729d12cf8e
Olgakorotkova90 <44948334+Olgakorotkova90@users.noreply.github.com>
2019-07-24 16:13:04 +0300

    adding base page class

---
+++ test_main_page.py
@@ -0,0 +1,8 @@
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    go_to_login_page(browser)
